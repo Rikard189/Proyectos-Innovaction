82d1bc769715a9586a928924935a9b50df84616a
Rikard189 <rikard189@gmail.com>
2017-03-14 09:46:22 -0600

    Adding access_level

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  enum access_level: [:estudiante , :profesor, :admin]
 end
