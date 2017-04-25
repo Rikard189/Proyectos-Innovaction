4344585d21a9403696938ad23fc928bad295d9f7
Rikard189 <rikard189@gmail.com>
2017-04-25 16:59:29 -0500

    I cant fix this, well i think i already fixed it

--- app/controllers/proyectos_controller.rb
+++ app/controllers/proyectos_controller.rb
@@ -1,10 +1,46 @@
 class ProyectosController < ApplicationController
+
+  def index
+    @proyectos = Proyecto.all
+  end
+
+  def show
+    @proyecto = Proyecto.find(params[:id])
+
+  end
+
   def new
+    @proyecto = Proyecto.new
+  end
+
+  def edit
+    @proyecto = Proyecto.find(params[:id])
   end
 
   def create
     @proyecto = Proyecto.new(proyecto_params)
-    @proyecto.save
+    if @proyecto.save
+      redirect_to @proyecto
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @proyecto = Proyecto.find(params[:id])
+
+    if @proyecto.update(article_params)
+      redirect_to @proyecto
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @proyecto = Proyecto.find(params[:id])
+    @proyecto.destroy
+    redirect_to proyectos_path
+
   end
 
   private
