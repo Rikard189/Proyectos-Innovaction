b2c7598f60bb85733b47c6c598a2d3e0277c27ee
Rikard189 <rikard189@gmail.com>
2017-04-26 17:36:01 -0500

    Fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :proyectos
+  resources :users do
+    resources :proyectos
+  end
   devise_for :users, controllers: {
         sessions: 'users/sessions'
       }
